2a88b90e726d9b6b0cd2856d9b722f8b4d568120
Bima Agung Setya Budi <bimaagungsetya@gmail.com>
2021-10-11 18:33:24 +0700

    prepare backend

---
+++ server.php
@@ -0,0 +1,26 @@
+<?php
+
+class server
+{
+
+    function __construct()
+    {
+        //koneksi menghubungkan ke database
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "db_tico";
+        $this->connect = new mysqli($server,$username,$password,$database);
+    }
+
+    function signin($username,$password){
+
+    }
+
+
+
+}
+
+
+
+?>
